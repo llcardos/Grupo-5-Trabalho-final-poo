1ca05c73684a4f009f99a354d3df209c5331ded6
NikolasGuerra <nikolasramos7@gmail.com>
2026-04-23 19:56:04 -0300

    feat: criação dos metodos para consulta do hospital

--- hospital/src/br/com/hospital/main/Main.java
+++ hospital/src/br/com/hospital/main/Main.java
@@ -1,16 +1,25 @@
 package br.com.hospital.main;
 
 import br.com.hospital.model.Faturamentos;
+import br.com.hospital.model.Hospital;
 import br.com.hospital.persistence.FaturaDao;
+import br.com.hospital.persistence.NotaFiscalDao;
 
 public class Main {
     public static void main(String[] args) {
         // new Menu().exibirMenuPrincipal();
 
-        FaturaDao faturaDao = new FaturaDao();
+        // FaturaDao faturaDao = new FaturaDao();
 
-        for (Faturamentos f : faturaDao.listar()) {
-            System.out.println(f);
-        }
+        // for (Faturamentos f : faturaDao.listar()) {
+        //     System.out.println(f);
+        // }
+        NotaFiscalDao notaFiscalDao = new NotaFiscalDao();
+        // for (Hospital hospital : notaFiscalDao.listarHospital()) {
+        // System.err.println(hospital);    
+        // }
+        Integer idBusca = 1;
+        Hospital hospital = notaFiscalDao.listarHospitalById(idBusca);
+        System.out.println(hospital);
     }
 }
--- hospital/src/br/com/hospital/persistence/NotaFiscalDao.java
+++ hospital/src/br/com/hospital/persistence/NotaFiscalDao.java
@@ -1,11 +1,58 @@
 package br.com.hospital.persistence;
 import br.com.hospital.conexao.ConnectionFactory;
+import br.com.hospital.model.Hospital;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotaFiscalDao {
     private Connection connection;
 
     public NotaFiscalDao() {
         connection = new ConnectionFactory().getConnection();
+    }
+
+    public List<Hospital> listarHospital(){
+        String sql = "select * from hospital";
+
+        List<Hospital> hospitais = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet result = stmt.executeQuery();
+            while ((result.next())) {
+                
+                Hospital hospital = new Hospital(result.getInt("id_hospital"), result.getString("nome"), result.getString("cnpj"));
+                hospitais.add(hospital);
+            }
+            result.close();
+            stmt.close();
+            connection.close();
+            
+        } catch (SQLException e) {
+            System.err.println("Problemas ao buscar Hospital");
+            e.printStackTrace();
+        }
+        return hospitais;
+    }
+
+    public Hospital listarHospitalById(int id){
+        String sql = " select * from hospital where id_hospital = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1,id);
+            ResultSet result = stmt.executeQuery();
+
+            if (result.next()) {
+                return new Hospital(result.getInt("id_hospital"), result.getString("nome"), result.getString("cnpj"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Problemas ao buscar hospital pelo id");
+        }
+        return null;
     }
 }
