bb75d26d5fd4f3208a9451919b7e6fd907b13070
pedrochuffart <pedropositivoone@gmail.com>
2026-04-23 21:46:33 -0300

    feat: Adicinando método Listar pacientes e listar pacientes by id

--- hospital/src/br/com/hospital/persistence/NotaFiscalDao.java
+++ hospital/src/br/com/hospital/persistence/NotaFiscalDao.java
@@ -1,6 +1,7 @@
 package br.com.hospital.persistence;
 import br.com.hospital.conexao.ConnectionFactory;
 import br.com.hospital.model.Hospital;
+import br.com.hospital.model.Pacientes;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -52,6 +53,44 @@ public class NotaFiscalDao {
             }
         } catch (SQLException e) {
             System.err.println("Problemas ao buscar hospital pelo id");
+        }
+        return null;
+    }
+
+    public List<Pacientes> listarPacientes(){
+        String sql = "select * from pacientes";
+        
+        List<Pacientes> pacientes = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet result = stmt.executeQuery();
+            while (result.next()) {
+                Pacientes paciente = new Pacientes(result.getInt("id_paciente"), result.getString("nome"), result.getString("data_nascimento"), result.getInt("id_credenciamento"), result.getInt("id_risco"), result.getInt("id_medico"));
+                pacientes.add(paciente);
+            }
+            result.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Problemas ao listar pacientes");
+            e.printStackTrace();
+        }
+        return pacientes;
+    }
+    
+    public Pacientes listarPacientesById(int id){
+        String sql = "select * from pacientes where id_paciente = ?";
+        
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet result = stmt.executeQuery();
+            if (result.next()) {
+                return new Pacientes(result.getInt("id_paciente"), result.getString("nome"), result.getString("data_nascimento"), result.getInt("id_credenciamento"), result.getInt("id_risco"), result.getInt("id_medico"));
+            }
+            
+        } catch (SQLException e) {
+            System.err.println("Problemas ao buscar paciente pelo id");
         }
         return null;
     }
