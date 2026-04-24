8ead351d4252caa70605750353900d65c149dba6
llcardos <137669141+llcardos@users.noreply.github.com>
2026-04-24 20:29:38 -0300

    feat: new method on faturaService

--- hospital/src/br/com/hospital/services/FaturaService.java
+++ hospital/src/br/com/hospital/services/FaturaService.java
@@ -32,4 +32,59 @@ public class FaturaService {
         System.out.println("IRPJ: " + totalIrpj);
         System.out.println("CSLL: " + totalCsll);
     }
+
+    public BigDecimal calculoImpostoIss(Integer id){
+        AliquotasImpostos al = new AliquotasImpostos();
+
+        double valorBruto = calculaValorBruto(id);
+        BigDecimal valorBrutoBD = new BigDecimal(valorBruto);
+
+        BigDecimal totalIss = valorBrutoBD.multiply(al.getISS());
+
+        return totalIss;
+    }
+
+    public BigDecimal calculoImpostoPis(Integer id){
+        AliquotasImpostos al = new AliquotasImpostos();
+
+        double valorBruto = calculaValorBruto(id);
+        BigDecimal valorBrutoBD = new BigDecimal(valorBruto);
+
+        BigDecimal totalPis = valorBrutoBD.multiply(al.getPIS());
+
+        return totalPis;
+    }
+
+    public BigDecimal calculoImpostoCofins(Integer id){
+        AliquotasImpostos al = new AliquotasImpostos();
+
+        double valorBruto = calculaValorBruto(id);
+        BigDecimal valorBrutoBD = new BigDecimal(valorBruto);
+
+        BigDecimal totalCofins = valorBrutoBD.multiply(al.getCOFINS());
+
+        return totalCofins;
+    }
+
+    public BigDecimal calculoImpostoIrpj(Integer id){
+        AliquotasImpostos al = new AliquotasImpostos();
+
+        double valorBruto = calculaValorBruto(id);
+        BigDecimal valorBrutoBD = new BigDecimal(valorBruto);
+
+        BigDecimal totalIrpj = valorBrutoBD.multiply(al.getIRPJ());
+
+        return totalIrpj;
+    }
+
+    public BigDecimal calculoImpostoCsll(Integer id){
+        AliquotasImpostos al = new AliquotasImpostos();
+
+        double valorBruto = calculaValorBruto(id);
+        BigDecimal valorBrutoBD = new BigDecimal(valorBruto);
+
+        BigDecimal totalCsll = valorBrutoBD.multiply(al.getCSLL());
+
+        return totalCsll;
+    }
 }
